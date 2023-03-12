82d4212efcb6f18cc01fe122536eb2b250b858e1
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-03-12 11:49:54 -0400

    Small steps to test the query and see if the DB actually connects

--- db/queries/product_query.js
+++ db/queries/product_query.js
@@ -6,8 +6,16 @@ const db = require('../connection');
 const getProducts = () => {
   return db.query('SELECT * FROM products;')
     .then(data => {
+      console.log('products line 9', data.rows);
       return data.rows;
     });
 };
 
 module.exports = { getProducts };
+
+// be sure to run the seed file.
+// console to see if this works at all Confirmed.
+// export module and then import into server.js to use a helper
+// then put this in server.js to see if that works using as a callback in the app(). Console from there
+// the put the exported OBJECT into EJS via <%
+// then maybe include AJAX for more dynamic site.
--- db/queries/users_query.js
+++ db/queries/users_query.js
@@ -3,16 +3,22 @@ const db = require('../connection');
 const getUsers = () => {
   return db.query('SELECT * FROM users;')
     .then(data => {
+      console.log('users line 6', data.rows);
       return data.rows;
     });
 };
+
+getUsers();
 
 // *** test run ***
 const userTest = () => {
   return db.query('SELECT first_name FROM users LIMIT 1;')
     .then(data => {
+      console.log('users line 15, LIMIT 1', data.rows);
       return data.rows;
     });
 };
+
+userTest();
 
 module.exports = { getUsers, userTest };
