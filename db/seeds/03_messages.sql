2990df447bfa341c41acecad0f4ead93639f415f
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-03-11 19:27:17 -0500

    Setup seeds for messages for auto timestamp

--- db/seeds/03_messages.sql
+++ db/seeds/03_messages.sql
@@ -1,4 +1,3 @@
--- Widgets table seeds here (Example)
-INSERT INTO widgets (name, user_id) VALUES ('Sprockets', 1);
-INSERT INTO widgets (name, user_id) VALUES ('Chains', 2);
-INSERT INTO widgets (name, user_id) VALUES ('Bearings', 2);
+INSERT INTO messages (id, sender_id, conversation_id, message_content,)
+VALUES
+()
