ece3df457c621dd489944aa514dd4b1969747b78
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-03-13 21:06:17 -0400

    Refactored products route to use helpers

--- routes/products_routes.js
+++ routes/products_routes.js
@@ -8,7 +8,9 @@
 const express = require('express');
 const router  = express.Router();
 const db = require('../db/connection');
+const productQueries = require('../db/queries/product_queries');
 
+/*
 router.get('/', (req, res) => {
   const query = `SELECT * FROM products`;
   console.log(query);
@@ -23,7 +25,17 @@ router.get('/', (req, res) => {
         .json({ error: err.message });
     });
 });
+*/
 
-
+router.get('/', (req, res) => {
+  productQueries.getProducts()
+    .then((products) => {
+    res.json(products);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error retrieving product data');
+    });
+});
 
 module.exports = router;
