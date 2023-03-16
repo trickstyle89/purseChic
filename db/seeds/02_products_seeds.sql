415e56ffae0b7425b0b58520e8861c7ee1718b9a
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-03-16 14:35:37 -0400

    Altered the products seed

--- db/seeds/02_products_seeds.sql
+++ db/seeds/02_products_seeds.sql
@@ -1,11 +1,10 @@
 INSERT INTO products (title, seller_id, price, description, product_photo, sold)
 VALUES
 (
-  'Chanel Classic Double Flap Bag', 1, 8000,'Condition: Very good. Creasing and scuffs on exterior and underneath flap, moderate wear on base corners, scuffs on interior flap and in interior. Wear and marks in interior, scratches on hardware.
-', './images/item1.png', FALSE
+  'Chanel Classic Double Flap Bag', 1, 8000,'Condition: Very good. Creasing and scuffs on exterior and underneath flap, moderate wear on base corners, scuffs on interior flap and in interior. Wear and marks in interior, scratches on hardware. The calfskin leather is smooth and luxurious, yet durable.', './images/item1.png', FALSE
 ),
 (
-  'Prada Black Nylon Flap Buckle Messenger Bag', 2, 920, 'This practical leather shoulder bag is accented by the enameled metal triangle logo and external pocket with buckle.', './images/item2.png', TRUE
+  'Prada Black Nylon Flap Buckle Messenger Bag', 2, 920, 'This practical leather shoulder bag is accented by the enameled metal triangle logo and external pocket with buckle. Excellent unused condition. Magnetic closure at centre top. Single zippered slit pocket on front. Made in Italy.', './images/item2.png', TRUE
   ),
 (
   'Dior Vintage Croissant Black Oblique Shoulder Bag ', 2, 1837,'This Dior bag is constructed of black canvas with monogram oblique print throughout. The bag features a croissant shape, black leather finishes, a top strap, silver tone hardware. top zip closure and black woven fabric lining with a slip and slip pockets.', './images/item3.png', FALSE
@@ -17,10 +16,10 @@ VALUES
   'Louis Vuitton Red Epi Leather Segur PM Handbag ', 2, 1696 ,'The Louis Vuitton Red Epi Leather Segur PM Bag features durable Epi leather with a gorgeous silvertone metal push-lock securing its flap closure. This PM size is the smaller size of the Segur family, which features double rolled handles and a rear slip pocket.', './images/item5.png', FALSE
   ),
 (
-  'Fendi Brown & Black Leather Runaway Bag', 1, 2049,'Brown leather Fendi Runaway bag with black leather trim. This bag was first seen on the Fall Winter 2017 runway and has a very straightforward and modern look with the trapezoid shape. The short & removable black leather top handle strap features the Fendi reversed ‘F’ signature logo hardware in gold metal on the left and silver on the right. The calfskin leather is smooth and luxurious, yet durable. Single zippered slit pocket on front. Magnetic closure at centre top. Grey suede interior with one large compartment, one zippered slit pocket and two small open slit pockets. Excellent unused condition. Made in Italy.', './images/item6.png', TRUE
+  'Fendi Brown & Black Leather Runaway Bag', 1, 2049,'Brown leather Fendi Runaway bag with black leather trim. This bag was first seen on the Fall Winter 2017 runway and has a very straightforward and modern look with the trapezoid shape. The short & removable black leather top handle strap features the Fendi reversed ‘F’ signature logo hardware.', './images/item6.png', TRUE
   ),
 (
-  'Louis Vuitton Indigo Coquelicot Epi Leather Dark Blue Twist Tote', 1, 1200,'This tote is crafted of textured epi leather in dark blue. The handbag features red glazing trim, contrasting yellow stitching, adjustable leather side straps, and polished silver hardware including a frontal LV turn lock. Dual top handles and an adjustable shoulder strap. The top opens to a partitioned blue microfiber interior with a center zipper and patch pockets.', './images/item7.png', FALSE
+  'Louis Vuitton Indigo Coquelicot Epi Leather Dark Blue Twist Tote', 1, 1200,'This tote is crafted of textured epi leather in dark blue. The handbag features red glazing trim, contrasting yellow stitching, adjustable leather side straps, and polished silver hardware including a frontal LV turn lock. Dual top handles and an adjustable shoulder strap.', './images/item7.png', FALSE
   ),
 (
   'Chanel Classic Double Flap Bag', 2, 11306,'This classic model nicknamed the Diana is constructed of supple diamond-quilted lambskin leather in pink. The shoulder bag features a waist-length polished gold chain-link and leather shoulder strap, Chanel CC turn lock closure on front flap and a burgundy leather interior with zipper.', './images/item8.png' , FALSE
