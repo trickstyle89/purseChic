b807e850538ab68a5b99a2a56bfee5c5c89ab4c0
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-03-16 14:06:26 -0400

    Chat bug fixed

--- routes/message_routes.js
+++ routes/message_routes.js
@@ -29,7 +29,6 @@ router.get('/', (req, res) => {
     return messageQueries.findChatMessages(chatId, senderId);
   })
   .then((messages) => {
-    console.log('Message_routes GET line32', messages);
     const templateVars = {
       messages: messages.rows,
       email };
@@ -50,24 +49,16 @@ router.post('/', (req, res) => {
     .then(result => {
       sender_id = result.sender_id;
       chat_id = result.chat_id;
-            console.log('Message POST line54 Message from req.body', message);
       return messageQueries.addMessage(sender_id, chat_id, message)
-       // .then(() => ({sender_id, chat_id}));
     })
-    .then(({sender_id, chat_id, message_content}) => {
-            console.log('line58', message_content);
-            console.log('Message POST line59 passing the id between methods', sender_id, chat_id);
+    .then(({sender_id, chat_id}) => {
       return messageQueries.findChatMessages(chat_id, sender_id);
     })
     .then(messages => {
-            console.log('Message POST right before templateVARS afterFindChat line62', messages);
       const templateVars = { messages: messages.rows , email };
-            console.log('Message: messages.rows line65', {messages: messages.rows} )
-            console.log('Message_routes POST templateVARS afterFindChat line66', templateVars);
       res.render('messages', templateVars);
     })
     .catch(err => {
-      console.log({ error: err.message });
       res.status(500).json({ error: err.message });
     });
 });
