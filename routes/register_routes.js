7472c5e2298a90d7f678bec9d223098fc9558df4
mrludovicc <nadal46@hotmail.com>
2023-03-17 10:16:21 -0400

    Fix an issue

--- routes/register_routes.js
+++ routes/register_routes.js
@@ -17,19 +17,20 @@ router.post('/', (req, res) => {
   const { first_name, last_name, email, password } = req.body;
   userQueries.getUserByEmail(email)
     .then((existingUser) => {
-      if (existingUser.rows.length > 0) {
-        return res.status(400).send('Email already exists');
-      } else {
-        return userQueries.addUser({ first_name, last_name, email, password })
-          .then((newUser) => {
-            req.session.email = newUser.email;
-            return res.redirect('/collection');
-          })
-          .catch((error) => {
-            console.error(error);
-            return res.status(500).send('Error registering new user data!');
-          });
-      }
+      // if (existingUser.rows.length > 0) {
+      //   return res.status(400).send('Email already exists');
+      // } else
+      // {
+      return userQueries.addUser({ first_name, last_name, email, password })
+        .then((newUser) => {
+          req.session.email = newUser.email;
+          return res.redirect('/collection');
+        })
+        .catch((error) => {
+          console.error(error);
+          return res.status(500).send('Error registering new user data!');
+        });
+      // }
     })
     .catch((error) => {
       console.error(error);
